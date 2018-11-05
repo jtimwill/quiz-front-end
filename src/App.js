34d7a46b3f5b681b7d333b4ebeaad7979495224c
Tim Williams <jtimwill@gmail.com>
2018-11-05 11:49:16 -0500

    Create basic layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
-
+import NavBar from './components/navBar';
+import Footer from './components/footer';
+import HomePage from './components/homePage';
+import ProtectedRoute from './components/protectedRoute';
+import NotFound from './components/notFound';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="custom-base-container">
+        <NavBar className="custom-navbar"/>
+        <main className="custom-container">
+          <Switch>
+            <Route path="/not-found" component={NotFound} />;
+            <Route path="/" component={HomePage} />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+        <Footer />
       </div>
     );
   }
---
+++ src/components/navBar.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+// import { getCurrentUser } from '../services/authService';
+
+class NavBar extends Component {
+  state = {
+    show: false
+  };
+
+  handleToggler = () => {
+    const show = !this.state.show;
+    this.setState({show});
+  };
+
+  hideDropdown = () => {
+    this.setState({show: false});
+  };
+
+  render() {
+    // const user = getCurrentUser();
+
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={this.handleToggler}>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <Link className="navbar-brand" to="/">QuizTracker</Link>
+        <div className="navbar-collapse" id="navbarNav">
+          <div
+            className={"navbar-nav custom-" + (this.state.show ? "show" : "hide")}
+            onClick={this.hideDropdown}
+          >
+            {/* {!user && (
+              <React.Fragment>
+                <NavLink className="nav-item nav-link" to="/login">
+                  Login
+                </NavLink>
+                <NavLink className="nav-item nav-link" to="/users/new">
+                  Register
+                </NavLink>
+              </React.Fragment>
+            )}
+            {user && ( */}
+              <React.Fragment>
+                <NavLink className="nav-item nav-link" to="/logout">
+                  Logout
+                </NavLink>
+                <NavLink className="nav-item nav-link" to="/users/me/show">
+                  Profile
+                </NavLink>
+                <NavLink className="nav-item nav-link" to="/exercises/index">
+                  QuizFinder
+                </NavLink>
+              </React.Fragment>
+            )}
+            {/* {user && user.admin && ( */}
+              <React.Fragment>
+                <NavLink className="nav-item nav-link" to="/users/index">
+                  Users
+                </NavLink>
+                <NavLink className="nav-item nav-link" to="/muscles/index">
+                  Quizzes
+                </NavLink>
+              </React.Fragment>
+            {/* )} */}
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
