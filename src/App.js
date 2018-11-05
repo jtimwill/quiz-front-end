713d7448e6ece162757fb338a4d9001162ec32fc
Tim Williams <jtimwill@gmail.com>
2018-11-05 12:09:52 -0500

    Update the routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import Footer from './components/footer';
 import HomePage from './components/homePage';
 import ProtectedRoute from './components/protectedRoute';
 import NotFound from './components/notFound';
+import Login from './components/login';
+import Logout from './components/logout';
+import UserIndex from './components/users/userIndex';
+import UserNew from './components/users/userNew';
+import UserEdit from './components/users/userEdit';
+import UserShow from './components/users/userShow';
 
 class App extends Component {
   render() {
@@ -13,6 +19,25 @@ class App extends Component {
         <NavBar className="custom-navbar"/>
         <main className="custom-container">
           <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/logout" component={Logout} />
+            <ProtectedRoute
+              path="/users/index"
+              component={UserIndex}
+              redirect_path="/users/me/show"
+              admin_required={true}
+            />
+            <Route path="/users/new" component={UserNew} />
+            <ProtectedRoute
+              path="/users/me/edit"
+              component={UserEdit}
+              redirect_path="/login"
+            />
+            <ProtectedRoute
+              path="/users/me/show"
+              component={UserShow}
+              redirect_path="/login"
+            />
             <Route path="/not-found" component={NotFound} />;
             <Route path="/" component={HomePage} />
             <Redirect to="/not-found" />
